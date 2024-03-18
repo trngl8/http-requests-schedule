526c1accbde782716cb48c35a8cd7bd22c105a6f
Red Rubber Duck <122934358+roman-duck@users.noreply.github.com>
2024-03-18 14:02:16 +0200

    Create data table, close #13

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -10,13 +10,16 @@ class HttpClient
 
     private HttpTransportInterface $transport;
 
+    private DataRepository $repository;
+
     private $logger;
 
-    public function __construct(HttpTransportInterface $transport, Logger $logger)
+    public function __construct(HttpTransportInterface $transport, Logger $logger, DataRepository $repository)
     {
         $this->response = new ResponseData(404, '404 Not Found');
         $this->logger = $logger;
         $this->transport = $transport;
+        $this->repository = $repository;
     }
 
     public function get(string $url): void
@@ -24,6 +27,7 @@ class HttpClient
         $result = $this->transport->get($url);
         $this->response = new ResponseData($this->transport->getStatusCode(), $result);
         $this->logger->info(sprintf('GET %s : %d', $url, $this->transport->getStatusCode()));
+        $this->repository->save($url, 'GET', $this->transport->getStatusCode());
     }
 
     public function post(string $url, array $data): void
@@ -31,6 +35,7 @@ class HttpClient
         $result = $this->transport->post($url, $data);
         $this->response = new ResponseData($this->transport->getStatusCode(), $result);
         $this->logger->info(sprintf('POST %s : %d', $url, $this->transport->getStatusCode()), $data);
+        $this->repository->save($url, 'POST', $this->transport->getStatusCode());
     }
 
     public function getResponse(): ResponseData
