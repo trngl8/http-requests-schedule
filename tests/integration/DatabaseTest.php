3ad5cbc98abefa1f87b901fed892e6a4cf2a0f2d
Red Rubber Duck <mvch@triangle.software>
2024-05-16 12:32:32 +0300

    coverage, refactoring

--- tests/integration/DatabaseTest.php
+++ tests/integration/DatabaseTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Integration;
 
 use App\Database;
+use App\Exception\DatabaseException;
 use PHPUnit\Framework\TestCase;
 
 class DatabaseTest extends TestCase
@@ -24,10 +25,45 @@ class DatabaseTest extends TestCase
         }
     }
 
+    public function testDatabaseExec(): void
+    {
+        $target = new Database('test');
+        $target->exec('DROP TABLE IF EXISTS not_exists');
+        $this->assertTrue(true);
+    }
+
     public function testDatabase(): void
     {
         $target = new Database('test');
-        $result = $target->fetch('requests',  ['id' => null]);
+        $result = $target->fetch('requests',  []);
         $this->assertEmpty($result);
+    }
+
+    public function testDatabaseInsertException(): void
+    {
+        $target = new Database('test');
+        $this->expectException(DatabaseException::class);
+        $target->insert('requests',  ['id' => 1]);
+    }
+
+    public function testDatabaseInsertSuccess(): void
+    {
+        $target = new Database('test');
+        $target->insert('requests',  ['method' => 'GET', 'url' => 'http://example.com']);
+        $ID = $target->lastInsertRowID();
+        $this->assertEquals(1, $ID);
+
+        $result = $target->fetch('requests',  ['id' => 1]);
+        $this->assertCount(1, $result);
+    }
+
+    public function testDatabaseUpdateSuccess(): void
+    {
+        $target = new Database('test');
+        $target->insert('requests',  ['method' => 'GET', 'url' => 'http://example.com']);
+        $target->update('requests',  ['method' => 'POST', 'url' => 'http://example.com'], ['id' => 1]);
+        $rec = $target->fetch('requests', ['id' => 1]);
+        $this->assertCount(1, $rec);
+        $this->assertEquals('POST', $rec[0]['method']);
     }
 }
