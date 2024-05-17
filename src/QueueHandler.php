31ffe52a34612a95c1912c635a9798d825bd3aee
Red Rubber Duck <mvch@triangle.software>
2024-05-17 21:56:27 +0300

    coverage

--- src/QueueHandler.php
+++ src/QueueHandler.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use App\Exception\TransportException;
+use App\Exception\ValidatorException;
 use Monolog\Handler\StreamHandler;
 use Monolog\Level;
 use Monolog\Logger;
@@ -12,13 +14,17 @@ class QueueHandler
 
     private $db;
 
+    private $client;
+
     public function __construct(?Logger $logger = null, ?Database $db = null)
     {
         $this->logger = $logger ?:
             (new Logger('test'))
-                ->pushHandler(new StreamHandler(__DIR__ . '/../var/logs/http.log', Level::Info))
+                ->pushHandler(new StreamHandler(__DIR__ . '../../var/logs/http.log', Level::Info))
         ;
         $this->db = $db ?: new Database('requests');
+        $transport = new CurlTransport();
+        $this->client = new HttpClient($transport);
     }
 
     public function run(): void
@@ -26,21 +32,21 @@ class QueueHandler
         $items = $this->db->fetch('requests', ['finished_at' => null], );
 
         foreach ($items as $item) {
-            $transport = new CurlTransport();
-            $client = new HttpClient($transport);
             try {
-                $result = $client->request($item['method'], $item['url']);
+                $result = $this->client->request($item['method'], $item['url']);
 
                 $this->db->exec('BEGIN');
                 $this->db->update('requests', ['finished_at' => date('Y-m-d H:i:s')], ['id' => $item['id']]);
                 $this->db->insert('responses', [
                     'request_id' => $item['id'],
-                    'code' => $client->getStatusCode(),
+                    'code' => $this->client->getStatusCode(),
                     'body' => htmlspecialchars($result),
                 ]);
                 $this->db->exec('COMMIT');
                 $this->logger->info(sprintf('Request %d processed', $item['id']));
-            } catch (\Exception $e) {
+            } catch (ValidatorException $e) {
+                $this->logger->warning($e->getMessage());
+            } catch (TransportException $e) {
                 $this->logger->error($e->getMessage());
             }
         }
--- tests/unit/QueueHandlerTest.php
+++ tests/unit/QueueHandlerTest.php
@@ -2,14 +2,34 @@
 
 namespace App\Tests\Unit;
 
+use App\Database;
+use App\Exception\ValidatorException;
 use App\QueueHandler;
+use Monolog\Logger;
 use PHPUnit\Framework\TestCase;
 
 class QueueHandlerTest extends TestCase
 {
+    public function testValidatorException(): void
+    {
+        $database = $this->createMock(Database::class);
+        $target = new QueueHandler(new Logger('test'), $database);
+        $database->method('fetch')->willReturn([
+            ['id' => 1, 'method' => 'NONE', 'url' => 'http://localhost'],
+            ['id' => 2, 'method' => 'GET', 'url' => '///']
+        ]);
+        $target->run();
+        $this->assertTrue(true);
+    }
+
     public function testSomething(): void
     {
-        $target = new QueueHandler();
+        $database = $this->createMock(Database::class);
+        $target = new QueueHandler(new Logger('test'), $database);
+        $database->method('fetch')->willReturn([
+            ['id' => 1, 'method' => 'GET', 'url' => 'http://localhost'],
+            ['id' => 2, 'method' => 'POST', 'url' => 'http://localhost']
+        ]);
         $target->run();
         $this->assertTrue(true);
     }
