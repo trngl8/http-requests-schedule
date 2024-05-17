0e41e1d91513d6a34dd9a6f6a4e47e756d8b4ca6
Red Rubber Duck <mvch@triangle.software>
2024-05-17 14:41:58 +0300

    console command

---
+++ src/Console/Export.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Console;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Export extends Command
+{
+    protected static $defaultName = 'app:export';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Export data')
+            ->setHelp('This command allows you to export data...');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        return Command::SUCCESS;
+    }
+}
