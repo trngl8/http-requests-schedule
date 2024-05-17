5acbacb4167d544dd1398b8f1abe7d61066f8c47
Red Rubber Duck <mvch@triangle.software>
2024-05-17 22:37:53 +0300

    fixed duplicates

--- tests/unit/QueueHandlerTest.php
+++ tests/unit/QueueHandlerTest.php
@@ -9,29 +9,15 @@ use PHPUnit\Framework\TestCase;
 
 class QueueHandlerTest extends TestCase
 {
-    public function testValidatorException(): void
-    {
-        $database = $this->createMock(Database::class);
-        $logger = $this->createMock(Logger::class);
-        $database->method('fetch')->willReturn([
-            ['id' => 1, 'method' => 'NONE', 'url' => 'http://localhost'],
-            ['id' => 2, 'method' => 'GET', 'url' => '///']
-        ]);
-
-        $target = new QueueHandler($database);
-        $target->setLogger($logger);
-        $target->run();
-
-        $this->assertEquals(0, $target->getProcessedCount());
-    }
-
     public function testSomething(): void
     {
         $database = $this->createMock(Database::class);
         $logger = $this->createMock(Logger::class);
         $database->method('fetch')->willReturn([
             ['id' => 1, 'method' => 'GET', 'url' => 'http://localhost'],
-            ['id' => 2, 'method' => 'POST', 'url' => 'http://localhost']
+            ['id' => 2, 'method' => 'PST', 'url' => 'http://localhost'],
+            ['id' => 3, 'method' => 'NONE', 'url' => 'http://localhost'],
+            ['id' => 4, 'method' => 'GET', 'url' => '///']
         ]);
 
         $target = new QueueHandler($database);
